862ae6540e9a020599287fa2c274fa970080c480
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-15 00:22:29 +0530

    server set up

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+// console.log("Loaded PORT:", process.env.PORT)
 
 const app = express()
+const PORT = process.env.PORT || 3000
+// console.log("PORT used by app.listen:", PORT)
 
 app.get("/", (req, res) => {
-  res.send("Server is ready")
+  // console.log("Received GET request at /")
+  res.send("Server is running properly now!")
 })
 
-app.listen(3000, () => {
-  console.log(`Server started at port 3000`)
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`)
 })
