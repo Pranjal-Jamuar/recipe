a2766c18eae11ef9365d5b04824ce7381cb18661
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-15 02:28:04 +0530

    add middleware auth, protect some routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 import express from "express"
 import User from "../models/User.js"
+import jwt from "jsonwebtoken"
+import { protect } from "../middleware/auth.js"
 
 const router = express.Router()
 
@@ -17,10 +19,12 @@ router.post("/", async (req, res) => {
       return res.status(400).json({ message: "User already exists!" })
 
     const user = await User.create({ userName, email, password })
+    const token = generateToken(user._id)
     res.status(201).json({
       _id: user._id,
       userName: user.userName,
       email: user.email,
+      token,
     })
   } catch (err) {
     res.status(500).json({ message: "Bad Request. Server Error!" })
@@ -36,14 +40,25 @@ router.post("/login", async (req, res) => {
     if (!user || (await user.matchPassowrd(password)))
       return res.status(401).json({ message: "Invalid Credentials!" })
 
+    const token = generateToken(user._id)
     res.json({
       id: user._id,
       userName: user.userName,
       email: user.email,
+      token,
     })
   } catch (err) {
     res.status(500).json({ message: "Bad Request. Server Error!" })
   }
 })
+
+router.get("/me", protect, async (req, res) => {
+  res.status(200).json(req.user)
+})
+
+// Generate JSON Web Token
+const generateToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" })
+}
 
 export default router
