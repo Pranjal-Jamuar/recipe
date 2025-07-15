45fe3dbe5a572305ca7808876c5022ff97cdeeed
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-15 09:30:33 +0530

    add login, register, home routes refactor: BrowserRouter in main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import Navbar from "./components/Navbar"
+import { Routes, Route } from "react-router-dom"
 import { AuthProvider } from "./context/AuthContext"
+import Home from "./pages/Home"
 
 function App() {
   return (
     <AuthProvider>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </AuthProvider>
   )
 }
