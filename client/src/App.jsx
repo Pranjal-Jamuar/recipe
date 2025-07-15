88b891ae293727eb42283c390f0e8fbd58cb9503
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-15 10:24:30 +0530

    add register component refactor: vite.config.js api route add routes in Navbar.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar"
 import { Routes, Route } from "react-router-dom"
 import { AuthProvider } from "./context/AuthContext"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </AuthProvider>
   )
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <nav className='bg-white shadow-md p-4'>
       <div className='max-w-7xl mx-auto flex justify-between items-center'>
-        <h1>Recipes</h1>
+        <Link to='/'>
+          <h1>Recipes</h1>
+        </Link>
         <div className='flex gap-x-4'>
-          <button>Login</button>
-          <button>Register</button>
+          <Link to='/login'>
+            <button>Login</button>
+          </Link>
+          <Link to='/register'>
+            <button>Register</button>
+          </Link>
         </div>
       </div>
     </nav>
